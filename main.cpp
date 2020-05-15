d35bc5abd8e6c6ec966d1f572a4e25b4c98749b6
vinicius <viniciusdpalves@gmail.com>
2020-05-15 17:14:03 -0300

    Function improved

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 
 using namespace std;
-void Change(int arr[],int i,int aux) {
-    if(i==0)return;
-    arr[i]=arr[i-1];
-    if(aux<arr[i-2])Change(arr,i-1,aux);
-    else{
-        arr[i-1]=aux;
+void Check(int arr[],int i,int aux) {
+    if(i==0||aux>=arr[i-1]){
+        arr[i]=aux;
         return;
+           }
+    if(aux<arr[i-1]){
+    arr[i]=arr[i-1];
+    Check(arr,i-1,aux);
+    return;
     }
     }
 void Insertion_Sort(int arr[],int n) {
     for(int i=1; i<n; i++) {
-        if(arr[i]<arr[i-1]) {
             int aux=arr[i];
-            Change(arr,i,aux);
-        }
+            Check(arr,i,aux);
     }
     return;
 }
 
 int main() {
-    int arr[]= {65,45,7,76,87,97,8,98,58,7};
+    int arr[]= {65,45,7,76,87,97,7,8,98,58,7};
     int n=sizeof(arr)/sizeof(arr[0]);
     Insertion_Sort(arr,n);
     cout << "Sorted Array : ";
